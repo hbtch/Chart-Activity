9a3768a5c9085db40f845114c728ec9d028295bd
hbtch <gulnaz.baembitova@mail.ru>
2024-03-19 14:32:21 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Chart } from "react-google-charts";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Chart
+    chartType="ScatterChart"
+    data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
+    width="100%"
+    height="400px"
+    legendToggle
+/>
     </div>
   );
 }
