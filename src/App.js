9918896a815eaac0cf68b0baa37a4c35d0c81e55
hbtch <gulnaz.baembitova@mail.ru>
2024-03-20 12:27:34 +0300

    tw

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 import { Chart } from "react-google-charts";
-function App() {
+
+export const data = [
+  ["Work", 45],
+  ["Sleep", 6],
+  ["Eat", 8],
+  ["Study", 30],
+  ["Cook", 7],
+  ["Relax", 4],
+];
+export const options = {
+  title: "Chart Activity",
+  legend: "none",
+  pieSliceText: "label",
+  slices: {
+    4: { offset: 0.2 },
+    12: { offset: 0.3 },
+    14: { offset: 0.4 },
+    15: { offset: 0.5 },
+  },
+};
+
+export function App() {
   return (
-    <div className="App">
     <Chart
-    chartType="ScatterChart"
-    data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
-    width="100%"
-    height="400px"
-    legendToggle
-/>
-    </div>
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
   );
 }
-
 export default App;
